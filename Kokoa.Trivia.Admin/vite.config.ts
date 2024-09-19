73373b51ea3a470ade05684ee534237b672dca99
Alexander Goussas <goussasalexander@gmail.com>
2024-09-18 22:39:44 -0500

    config: Update application base path

--- Kokoa.Trivia.Admin/vite.config.ts
+++ Kokoa.Trivia.Admin/vite.config.ts
@@ -1,6 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
 	plugins: [sveltekit()]
-});
+}));
